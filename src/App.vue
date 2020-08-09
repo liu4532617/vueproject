08205d1c96b6b2d63053547fd86941a4a7ba0b5d
LAPTOP-MI81TCMU\liuli <liulin4532617@163.com>
2020-08-09 21:48:27 +0800

    晚上提交

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import main from './components/main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    main
   }
 }
 </script>
---
+++ src/components/main.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+     asdfsdf {{ msg }}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data() {
+    return {
+      msg:"asdf"
+    }
+  }
+}
+</script>
+
+ 
